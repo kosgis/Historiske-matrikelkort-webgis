fca8bfe5163e5d593cc03488faa269b943ac4cf3
Hasse Hauch <hasse.hauch@gmail.com>
2014-03-13 14:39:29 +0100

    Første html + js

---
+++ js/matrikelkort.js
@@ -0,0 +1,124 @@
+var matrikelplan_01I_800_1921 = new L.TileLayer('http://54.229.79.223:8888/v2/Plan_01I_1-800_1921/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+       // zIndex: -10
+});
+
+var matrikelplan_01II_800_1932 = new L.TileLayer('http://54.229.79.223:8888/v2/Plan_01II_1-800_1932/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+       // zIndex: -10
+});
+
+var matrikelplan_02I_800_1921 = new L.TileLayer('http://54.229.79.223:8888/v2/Plan_02I_1-800_1921/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+       // zIndex: -10
+});
+
+var matrikelplan_02II_800_1921 = new L.TileLayer('http://54.229.79.223:8888/v2/Plan_02II_1-800_1921/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20,
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+       // zIndex: -10
+});
+
+var matrikelplan_03_800_1921 = new L.TileLayer('http://54.229.79.223:8888/v2/Plan_03_1-800_1921/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+       // zIndex: -10
+});
+
+var matrikelplan_04_800_1921 = new L.TileLayer('http://54.229.79.223:8888/v2/Plan_04_1-800_1921/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+       // zIndex: -10
+});
+
+var matrikelplan_05I_800_1921 = new L.TileLayer('http://54.229.79.223:8888/v2/Plan_05I_1-800_1921/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+       // zIndex: -10
+});
+
+var matrikelplan_05II_800_1921 = new L.TileLayer('http://54.229.79.223:8888/v2/Plan_05II_1-800_1921/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+       // zIndex: -10
+});
+var matrikelplan_06I_800_1921 = new L.TileLayer('http://54.229.79.223:8888/v2/Plan_06I_1-800_1921/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+       // zIndex: -10
+});
+
+var matrikelplan_06II_800_1921 = new L.TileLayer('http://54.229.79.223:8888/v2/Plan_06II_1-800_1921/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+       // zIndex: -10
+});
+
+
+
+
+
+
+
+
+
+
+var kort_1932 = new L.TileLayer('http://54.229.79.223:8888/v2/kort1932/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+        //zIndex: -30
+});
+
+var matrikeler = new L.TileLayer('http://54.229.79.223:8888/v2/Matrikler/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+        zIndex: 1
+});
+var bygning = new L.TileLayer('http://54.229.79.223:8888/v2/FOT_bygninger/{z}/{x}/{y}.png', {
+		tms: false,
+		minZoom: 13, 
+        maxZoom: 20, 
+		opacity: 1,
+        attribution: '&copy; Frederiksberg Kommune', 
+        zIndex: 2
+});
+
